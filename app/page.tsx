3c33e7e1b1ae5e3af5e516a0875e04d3b356d312
Mansour Qaderi <mansourqaderi21@gmail.com>
2025-08-28 13:25:05 +0430

    remove previous metadata

--- app/page.tsx
+++ app/page.tsx
@@ -11,37 +11,9 @@ import { Sponsors } from "@/components/sponsors";
 import { Teams } from "@/components/team";
 import { Testimonial } from "@/components/testimonial";
 
-export const metadata = {
-  title: "Shadcn - Landing template",
-  description: "Free Shadcn landing page for developers",
-  openGraph: {
-    type: "website",
-    url: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
-    images: [
-      {
-        url: "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Shadcn - Landing template",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Shadcn - Landing template",
-    description: "Free Shadcn landing page for developers",
-    images: [
-      "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
-    ],
-  },
-};
-
 export default function Home() {
   return (
-    <div className="w-full flex flex-col lg:max-w-screen-xl mx-auto gap-20 md:gap-32 px-2 py-10 sm:py-20 md:py-32">
+    <div className="w-full flex flex-col lg:max-w-screen-xl mx-auto gap-20 md:gap-32 py-10 sm:py-20 md:py-32">
       <Hero />
       <Sponsors />
       <Benefits />
